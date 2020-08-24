69cd5b781201336097534dd003a7af43455ac2f1
JordonGoodsir <jordon.goodsir1@gmail.com>
2020-08-24 14:37:46 +1000

    prompt practice complete

---
+++ day_1/js_prompt_practice/browser.js
@@ -0,0 +1,17 @@
+let user = "tom";  
+
+if (user === "tom") { 
+    console.log("hello tom") 
+} else { 
+    console.log("hello someone other than tom") 
+} 
+
+const min_age = 18;  
+let age = 3
+
+if (age < min_age) { 
+    console.log("too yung") 
+} else { 
+    console.log("Please come gamble") 
+
+}
