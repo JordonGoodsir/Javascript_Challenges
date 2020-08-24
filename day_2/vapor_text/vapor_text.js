f91a0c9a7e2e9985dfd47990fc358df01e25ded4
JordonGoodsir <jordon.goodsir1@gmail.com>
2020-08-25 09:19:24 +1000

    vapor text complete

---
+++ day_2/vapor_text/vapor_text.js
@@ -0,0 +1,12 @@
+// Your code here
+
+toVaporwave = (text) => { 
+    console.log(text.split("").join(" "))
+}
+
+toVaporwave("good things are good")
+
+// Dont Change This!
+// module.exports = {
+//     toVaporwave
+// }
