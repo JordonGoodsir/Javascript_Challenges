95ba35a22f8de234dcc5413b1d03eac6d7be7cb0
JordonGoodsir <jordon.goodsir1@gmail.com>
2020-08-24 14:44:23 +1000

    bigger number challenge complete

---
+++ day_1/bigger_number/bigger_number.js
@@ -0,0 +1,9 @@
+bigger = (num1,num2) => { 
+    if (num1 > num2) { 
+        console.log(`${num1} is bigger than ${num2}`)
+    } else { 
+        console.log(`${num2} is bigger than ${num1}`)
+    }
+} 
+
+bigger(3,2)
