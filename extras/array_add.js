229f10542932e4070e7373fff1d7096979f76ac4
JordonGoodsir <jordon.goodsir1@gmail.com>
2020-08-29 12:16:37 +1000

    array and regex methods challenge complete

---
+++ extras/array_add.js
@@ -0,0 +1,35 @@
+// Use an appropriate array iterator method to do the following challenges:
+
+// 1. Add 1
+
+// Write a function called addOne that takes an array of numbers and returns an array with each number incremented by 1.
+
+// E.g. addOne([1,2,3]), should return [2,3,4] 
+
+const addOne = arr => { 
+    console.log(arr.map(x => x + 1))
+} 
+
+// addOne([1,2,3]) 
+
+// 2. Search
+
+// Write a function called search that takes two arguments:
+
+// an array of strings
+// a string
+// The function should return the values in the array that contain the string argument.
+
+// E.g. findIt([“one”, “two”, “bone”], “on”), should return [“one”, “bone”] 
+
+const addItem = (arr,item) => {   
+
+    console.log(arr.filter(( arrItems => {
+        let finder = item.split();
+        return eval(`/${finder}/`).test(arrItems);
+    })));
+
+
+} 
+
+addItem(["ther","good","the"],"the")
