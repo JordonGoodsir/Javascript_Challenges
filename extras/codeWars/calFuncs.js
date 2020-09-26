199c8db6171a008d156d6ad4391abecf9f3008dc
JordonGoodsir <jordon.goodsir1@gmail.com>
2020-09-26 11:08:53 +1000

    code wars challenge complete

---
+++ extras/codeWars/calFuncs.js
@@ -0,0 +1,107 @@
+
+
+zero = (operator = 0) => { 
+if (operator != 0) { 
+    console.log(Math.floor(eval(0 + operator))) 
+}
+return operator
+
+} 
+
+one = (operator = 1) => {     
+
+if (operator != 1) { 
+    console.log(Math.floor(eval(1 + operator))) 
+}
+return operator
+  
+} 
+
+two = (operator = 2) => {  
+
+   if (operator != 2) { 
+    console.log(Math.floor(eval(2 + operator))) 
+}
+return operator
+
+    
+} 
+
+three = (operator = 3) => {  
+
+   if (operator != 3) { 
+    console.log(Math.floor(eval(3 + operator))) 
+}
+return operator
+
+   
+} 
+
+four = (operator = 4) => {  
+ 
+   if (operator != 4) { 
+    console.log(Math.floor(eval(4 + operator))) 
+}
+return operator
+   
+} 
+
+five = (operator = 5) => {  
+
+   if (operator != 5) { 
+    console.log(Math.floor(eval(5 + operator))) 
+}
+return operator 
+  
+} 
+
+six = (operator = 6) => { 
+ 
+  if (operator != 6) { 
+    console.log(Math.floor(eval(6 + operator))) 
+}
+return operator
+   
+} 
+
+seven = (operator = 7) => {  
+
+   if (operator != 7) { 
+    console.log(Math.floor(eval(7 + operator))) 
+}
+return operator
+
+   
+} 
+
+eight = (operator = 8) => {  
+
+   if (operator != 8) { 
+    console.log(Math.floor(eval(8 + operator))) 
+}
+return operator
+   
+} 
+
+nine = (operator = 9) => {  
+   if (operator != 9) { 
+    console.log(Math.floor(eval(9 + operator))) 
+}
+return operator
+   
+}
+
+plus = (num) => {  
+ return "+ " + num
+}
+minus = (num) => { 
+ return "- " + num
+}
+times = (num) => { 
+ return "* " + num
+}
+dividedBy = (num) => {  
+ return "/" + num
+} 
+
+eight(dividedBy(two()))
