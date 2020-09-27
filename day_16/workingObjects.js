846b2287bbab0e2c8c1b9e477aa6201aabb825bb
JordonGoodsir <jordon.goodsir1@gmail.com>
2020-09-28 09:11:47 +1000

    morning challenges 1/2 complete

---
+++ day_16/workingObjects.js
@@ -0,0 +1,38 @@
+let startingObject = {
+	name: "Alex",
+	age: 24,
+	favourites: {
+		game: "Super Smash Bros. Ultimate",
+		drink: "Orange Vanilla Coca Cola",
+		food: "Chocolate Orange Honeycomb",
+		activity: "Playing videogames",
+		programmingLanguage: "C#"
+	},
+	location: "NSW",
+	coworkers: ["Nandini", "Carl", "Jairo"]
+}
+
+// Step 01.
+//	Modify the code below so that it 
+//	accesses the favourite game from the startingObject.
+console.log(`Alex's favourite game is ${startingObject.favourites.game}`);
+
+
+// Step 02.
+//	Add the missing coworkers to 
+//	the "coworkers" property on startingObject. 
+
+startingObject.coworkers.push("Alex") 
+
+
+// Step 03.
+//	Delete the age property from startingObject.
+
+delete startingObject.age 
+
+
+
+module.exports = {
+	startingObject
+
+}
