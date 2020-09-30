95c19e0bf765e0ef4160c6439dc6ef86cd5495f6
JordonGoodsir <jordon.goodsir1@gmail.com>
2020-09-30 10:05:16 +1000

    jest test added

---
+++ allNodeServers/test/jokes.jest.js
@@ -0,0 +1,23 @@
+let randomJokes = require("../src/jokes")  
+
+
+
+test("empty func call gives value", () =>{ 
+   expect(Boolean(randomJokes.returnRandomJokes())).toBe(true)
+});  
+
+test("2 or greater func call is array", () =>{ 
+   expect(Array.isArray(randomJokes.returnRandomJokes(2))).toBe(true)
+});  
+
+test("error throw on nan", () =>{ 
+    expect(randomJokes.returnRandomJokes("the")).toThrow()
+ });    
+
+ test("error throw on decimal", () =>{ 
+    expect(randomJokes.returnRandomJokes("the")).toThrow()
+ });   
+ 
+ test("error throw on nan", () =>{ 
+    expect(randomJokes.returnRandomJokes(1.2)).toThrow()
+ });
