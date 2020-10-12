be4997b8134ab267ec8470cd0dd6a0d2df55429c
JordonGoodsir <jordon.goodsir1@gmail.com>
2020-10-12 14:33:06 +1100

    morning challenge 2 complete

---
+++ day_20/possibleSum.js
@@ -0,0 +1,14 @@
+function sumIfPossible(arr) { 
+ let filterdArr = arr.filter(v=> typeof v == "number")  
+ let sumArr = filterdArr.reduce((a,b) => a+b)
+ 
+  let ans = { 
+      sum: sumArr, 
+      skip: arr.length - filterdArr.length
+  } 
+
+  return ans 
+}   
+
+sumIfPossible(["number", 234, 34, "string", -9])
+
