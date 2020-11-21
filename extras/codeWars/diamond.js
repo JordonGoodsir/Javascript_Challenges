980a3afa432387859845d352db6fe59341a207cd
JordonGoodsir <jordon.goodsir1@gmail.com>
2020-11-21 17:04:37 +1100

    codewars challenge complete

---
+++ extras/codeWars/diamond.js
@@ -0,0 +1,42 @@
+function diamond(n){ 
+    if(n%2 ==0){ 
+        return null
+    } else if(n== 1){ 
+        return `*\n`
+    }
+
+
+    ansArr=[]   
+
+    realN= n 
+    ansArr.push(`${"*".repeat(n)}\n`) 
+    do{    
+        n = n - 2     
+        
+        if(n==1){  
+            ansArr.push(`${" ".repeat(Math.floor(realN)/ 2)}${"*".repeat(n)}\n`) 
+            ansArr.unshift(`${" ".repeat(Math.floor(realN)/ 2)}${"*".repeat(n)}\n`)
+        } else {  
+            ansArr.push(`${" ".repeat(Math.floor(realN)/ n)}${"*".repeat(n)}\n`) 
+            ansArr.unshift(`${" ".repeat(Math.floor(realN)/ n)}${"*".repeat(n)}\n`) 
+        } 
+    } 
+    while(n > 1)   
+
+    console.log(ansArr.join(""))
+
+    return ansArr.join("")
+  }  
+
+  diamond(9)    
+
+
+
+  
+
+// what should look like
+//   console.log(' *\n***\n *\n')  
+
+// mine
+//   console.log(' *\n ***\n *\n')
+
