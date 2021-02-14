94c2f2cdaca14c2d83bfdc47fe7762178a4683c1
JordonGoodsir <jordon.goodsir1@gmail.com>
2021-02-15 10:53:14 +1100

    callenge complete

--- extras/codeWars/findDomain.js
+++ extras/codeWars/findDomain.js
@@ -1,8 +1,16 @@
-function domainName(url){
-    
-    let searchBefore = url.search(".com")
-    let serchAfter = url.indexOf(".") + 1
-    return url.substring(searchBefore, serchAfter)
-  } 
+function domainName(url) {
+  // let searchBefore = url.search(".com")
+  let searchBefore = url.includes("www.")
+    ? url.indexOf(".", 4)
+    : url.indexOf(".", 1);
 
-  domainName("https://www.cnet.com")
+  
+    let searchAfter =
+    url.charAt(3) == "." ? url.indexOf(".") + 1 : url.indexOf("/") + 2;
+  
+
+
+  return console.log(url.substring(searchBefore, searchAfter));
+}
+
+domainName("https://www.i2a31a-7i1slin4.us/archive/");
