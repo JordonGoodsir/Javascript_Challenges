a382def41bcdb81d311b7eb7b13cf3277d19fccf
JordonGoodsir <jordon.goodsir1@gmail.com>
2021-02-16 10:28:35 +1100

    challenge started

---
+++ extras/codeWars/numSum.js
@@ -0,0 +1,12 @@
+function high(x){ 
+    let alphabet = 'abcdefghijklmnopqrstuvwxyz'.split('');   
+    let numbers = []
+
+    x.split("").forEach((v,i) => {
+        v.split("") 
+        // search alphatbet for letter and get index + 1
+    });
+} 
+
+high('man i need a taxi up to ubud')
+
