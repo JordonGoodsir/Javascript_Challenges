f4866c0b35c2afa2f94e0977825fd578efd20542
JordonGoodsir <jordon.goodsir1@gmail.com>
2021-02-16 11:34:37 +1100

    challenge complete

--- extras/codeWars/numSum.js
+++ extras/codeWars/numSum.js
@@ -1,11 +1,23 @@
 function high(x){ 
     let alphabet = 'abcdefghijklmnopqrstuvwxyz'.split('');   
-    let numbers = []
+    let numbers = [] 
+    let total = 0 
 
-    x.split("").forEach((v,i) => {
-        v.split("") 
-        // search alphatbet for letter and get index + 1
-    });
+    x.split(" ").forEach((v,i) => {
+        
+        
+        v.split("").map((letter,i)=>{  
+            let letterNum = alphabet.indexOf(letter) + 1 
+            total += letterNum    
+         })  
+
+        numbers.push(total) 
+
+        total = 0
+
+    });  
+   return x.split(" ")[numbers.indexOf(Math.max(...numbers))]
+
 } 
 
 high('man i need a taxi up to ubud')
