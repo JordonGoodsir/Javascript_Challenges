e79f0a9848ec25c1340fa2955bcfd4d4cb5f62fa
JordonGoodsir <jordon.goodsir1@gmail.com>
2021-02-24 09:58:45 +1100

    first part of challenge complete

--- extras/codeWars/firstRepeating.js
+++ extras/codeWars/firstRepeating.js
@@ -1,12 +1,40 @@
 function firstNonRepeatingLetter(s) { 
     let letters = {} 
 
-    s.split("").forEach((v,i) => {  
-    letters[v] ? letters[v] = letters[v] + 1 : letters[v] = 1 
-    }) 
+    s.split("").forEach((v,i) => { 
+        
+        if (v == v.toUpperCase()) {   
+            if (letters[v.toLowerCase()]){  
+                // letters[v.toLowerCase()] = letters[v.toLowerCase()] + 1     
+                // console.log(typeof letters[v.toLowerCase()] + 1) 
+                // console.log(letters[v.toLowerCase()][0] + 1) 
+                // letters[v.toLowerCase()][0] = [letters[v.toLowerCase()] + 1 ]  
+                letters[v.toLowerCase()][0] = letters[v.toLowerCase()][0] + 1
 
-   return Object.keys(letters)[Object.values(letters).indexOf(1)] ? console.log(Object.keys(letters)[Object.values(letters).indexOf(1)]) : ""
+            } else {  
+                // letters[v.toLowerCase()] = 1     
+                letters[v.toLowerCase()] = [1]     
+                letters[v.toLowerCase()].push(true)
+            }
+        } else {  
+            // letters[v] ? letters[v] = letters[v] + 1 : letters[v] = [1]  
+            
+            // letters[v] ? letters[v][0] = letters[v][0] + 1 : letters[v] = [1]  
+            letters[v] ?  letters[v][0] = letters[v][0] + 1 : letters[v] = [1] 
 
+
+        }
+    
+    })  
+
+   
+
+    console.log(letters)
+ 
+
+//    return Object.keys(letters)[Object.values(letters).indexOf(1)] ? console.log(Object.keys(letters)[Object.values(letters).indexOf(1)]) : "" 
+
+   
   }
 
-  firstNonRepeatingLetter('s,ress')
+  firstNonRepeatingLetter('sTtrReSS')
