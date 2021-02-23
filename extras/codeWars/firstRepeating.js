ca3cece1fa511f5d213e29fac58de5994c4664b6
JordonGoodsir <jordon.goodsir1@gmail.com>
2021-02-24 10:30:39 +1100

    challenge complete

--- extras/codeWars/firstRepeating.js
+++ extras/codeWars/firstRepeating.js
@@ -5,36 +5,39 @@ function firstNonRepeatingLetter(s) {
         
         if (v == v.toUpperCase()) {   
             if (letters[v.toLowerCase()]){  
-                // letters[v.toLowerCase()] = letters[v.toLowerCase()] + 1     
-                // console.log(typeof letters[v.toLowerCase()] + 1) 
-                // console.log(letters[v.toLowerCase()][0] + 1) 
-                // letters[v.toLowerCase()][0] = [letters[v.toLowerCase()] + 1 ]  
+
                 letters[v.toLowerCase()][0] = letters[v.toLowerCase()][0] + 1
 
             } else {  
-                // letters[v.toLowerCase()] = 1     
                 letters[v.toLowerCase()] = [1]     
                 letters[v.toLowerCase()].push(true)
             }
-        } else {  
-            // letters[v] ? letters[v] = letters[v] + 1 : letters[v] = [1]  
-            
-            // letters[v] ? letters[v][0] = letters[v][0] + 1 : letters[v] = [1]  
+        } else {   
             letters[v] ?  letters[v][0] = letters[v][0] + 1 : letters[v] = [1] 
-
-
         }
     
     })  
 
    
 
-    console.log(letters)
- 
+let ans = ""
 
-//    return Object.keys(letters)[Object.values(letters).indexOf(1)] ? console.log(Object.keys(letters)[Object.values(letters).indexOf(1)]) : "" 
+
+let i = 0  
+
+
+
+Object.values(letters).forEach((v,i)=>{  
+    if (Object.values(letters)[i][0] == 1) { 
+        Object.values(letters)[i].length == 2 ? ans = [i, true] : ans = [i]
+   }
+}) 
+
+ return ans == "" ? ans : (ans.length == 2 ? console.log(Object.keys(letters)[ans[0]].toUpperCase()) : console.log(Object.keys(letters)[ans[0]]))
+
+ 
 
    
   }
 
-  firstNonRepeatingLetter('sTtrReSS')
+  firstNonRepeatingLetter(`stress`)
