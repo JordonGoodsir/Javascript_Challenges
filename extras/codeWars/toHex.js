9a75a2c83f45dbeb3c3725c515d7dd2227e6279c
JordonGoodsir <jordon.goodsir1@gmail.com>
2021-02-25 17:34:58 +1100

    challenge complete

--- extras/codeWars/toHex.js
+++ extras/codeWars/toHex.js
@@ -1,9 +1,12 @@
 function rgb(r, g, b){
     let ans = []
-  ans.push(r.toString(16)) 
-  ans.push(g.toString(16)) 
-  ans.push(b.toString(16))
+  ans.push(r.toString(16).toUpperCase()) 
+  ans.push(g.toString(16).toUpperCase()) 
+  ans.push(b.toString(16).toUpperCase()) 
+  
+  console.log(ans.join(""))
       
-  }
+  } 
 
-console.log(hexString)
+  rgb(300 ,255, 255 )
+
