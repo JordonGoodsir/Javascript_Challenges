d78db91d20926daebeceebebb915a55cffbcb101
JordonGoodsir <jordon.goodsir1@gmail.com>
2021-03-04 15:38:42 +1100

    challenge cleaned up

--- extras/codeWars/sameStructure.js
+++ extras/codeWars/sameStructure.js
@@ -1,62 +1,33 @@
 Array.prototype.sameStructureAs = function (other) {
-  
-    let toMatch = [] 
+  let original = [];
+  let second = [];
 
-    // toMatch.push(this.length) 
-    
-    //  this.forEach((v,i)=>{ 
+  JSON.stringify(this)
+    .split("")
+    .forEach((v, i) => {
+      if (
+        (JSON.stringify(this).split("")[i - 1] == `"` && v == "[") ||
+        (v == "]" && JSON.stringify(this).split("")[i + 1] != undefined)
+      ) {
+        original.push("");
+      } else {
+        original.push(v.replace(/[^\[\], ]+/, ""));
+      }
+    });
 
-    //  })       
+  JSON.stringify(other)
+    .split("")
+    .forEach((v, i) => {
+      if (
+        (JSON.stringify(other).split("")[i - 1] == `"` && v == "[") ||
+        (v == "]" && JSON.stringify(this).split("")[i + 1] != undefined)
+      ) {
+        original.push("");
+      } else {
+        second.push(v.replace(/[^\[\], ]+/, ""));
+      }
+    });
 
-    // let ans = []
-
-    // let hello = "h h h ]           [" 
-
-    // hello.split("").forEach((v,i) =>{ 
-    //    ans.push(v.replace(/[^\[\] ]+/ ,"bye"))
-    // })
- 
-    // console.log(ans.join(""))  
-
-    let original = []  
-    let second = []
-
-    JSON.stringify(this).split("").forEach((v,i) => { 
-
-        // console.log(JSON.stringify(this).split("")[i])
-        if(JSON.stringify(this).split("")[i - 1] == `"` && v == "[" || v == "]" && JSON.stringify(this).split("")[i + 1] != undefined) {  
-            original.push("")  
-        } else { 
-            original.push(v.replace(/[^\[\], ]+/ ,""))
-        }
-    })  
-    
-    JSON.stringify(other).split("").forEach((v,i) => {  
-        
-        if(JSON.stringify(other).split("")[i - 1] == `"` && v == "[" || v == "]" && JSON.stringify(this).split("")[i + 1] != undefined) { 
-            original.push("")  
-        } else { 
-            second.push(v.replace(/[^\[\], ]+/ ,""))
-
-        }
-    })   
-
-
-    console.log(original.join("")) 
-    console.log(second.join(""))
-
-
-
-    console.log(JSON.stringify(original.join(""))==JSON.stringify(second.join("")))  
-
-    return JSON.stringify(original.join(""))==JSON.stringify(second.join(""))
-
-
-
-
-    // console.log(JSON.stringify([[]]).replaceAll("[", "p"))
-    // console.log(JSON.stringify([[]])==JSON.stringify([])) 
-
-
-}; 
-[1,'[',']'].sameStructureAs(['[',']',1]);
+  return JSON.stringify(original.join("")) == JSON.stringify(second.join(""));
+};
+[1, "[", "]"].sameStructureAs(["[", "]", 1]);
