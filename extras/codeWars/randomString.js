571734cd6faa3e175cdcd9a959927434e387afa6
JordonGoodsir <jordon.goodsir1@gmail.com>
2021-03-15 23:00:24 +1100

    challenge complete

---
+++ extras/codeWars/randomString.js
@@ -0,0 +1,17 @@
+// way 1
+function makeid(length) {
+    var result           = '';
+    var characters       = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
+    var charactersLength = characters.length;
+    for ( var i = 0; i < length; i++ ) {
+       result += characters.charAt(Math.floor(Math.random() * charactersLength));
+    }
+    return result;
+ }
+ 
+ console.log(makeid(5)); 
+
+//  way 2 
+
+let r = Math.random().toString(36).substring(7);
+console.log("random", r);
