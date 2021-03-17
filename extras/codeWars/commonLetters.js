be2b2f2fc5eb6bc22d0fae7452ce270504fa43d8
JordonGoodsir <jordon.goodsir1@gmail.com>
2021-03-17 18:13:25 +1100

    challenge v1 finished

--- extras/codeWars/commonLetters.js
+++ extras/codeWars/commonLetters.js
@@ -1,3 +1,14 @@
-function LCS(x, y) {
-    //TODO 
-  }
+function LCS(x, y) {  
+
+    let common = []
+
+    x.split("").forEach((v,i)=>{  
+        if (y.split("").includes(v)){ 
+            common.push(v)
+        }
+    }) 
+    console.log([...new Set(common)])
+    return [...new Set(common)]
+  } 
+
+  LCS("abcdef", "abc") 
