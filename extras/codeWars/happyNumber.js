4a608adb68a9a4a3d6f9bec3436b894233fc8200
JordonGoodsir <jordon.goodsir1@gmail.com>
2021-03-23 21:43:44 +1100

    happy number sol 2 complete

--- extras/codeWars/happyNumber.js
+++ extras/codeWars/happyNumber.js
@@ -19,4 +19,13 @@ function happy(n) {
 			sum = getSingleDigitSqrSum(sum);
 	}
 	return sum == 1 ? true:false;
+} 
+
+// sol 2 
+
+function happy(n) {
+	if (n == 1) return true;
+	if (n == 4) return false;
+	return happy([...n.toString()]
+		.reduce((sum, v) => Math.pow(Number(v), 2) + sum, 0))
 }
