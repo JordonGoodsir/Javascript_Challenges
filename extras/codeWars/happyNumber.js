e25d496f5add8cd8ebc9a75c72d8450e70bb64b0
JordonGoodsir <jordon.goodsir1@gmail.com>
2021-03-23 21:39:58 +1100

    solutiuon 1 complete

--- extras/codeWars/happyNumber.js
+++ extras/codeWars/happyNumber.js
@@ -2,4 +2,21 @@
 
 // happy(11) ➞ false
 
-// happy(107) ➞ false
+// happy(107) ➞ false 
+
+function happy(n) {
+	function getSingleDigitSqrSum(num) {
+		var sqrSum = String(num).split("").reduce(
+			function(pv, cv, ci) {
+				var numValue = parseInt(cv);
+				return pv + numValue*numValue;
+			}, 0
+		);
+		return sqrSum;
+	}
+	var sum = getSingleDigitSqrSum(n);
+	while(sum > 9) {
+			sum = getSingleDigitSqrSum(sum);
+	}
+	return sum == 1 ? true:false;
+}
