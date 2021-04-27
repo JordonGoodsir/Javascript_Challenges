b19f46573c725f815c86434814781cf8aa745c7b
JordonGoodsir <jordon.goodsir1@gmail.com>
2021-04-27 11:18:25 +1000

    challenge complete :)

--- extras/codeWars/decodeMorseCode.js
+++ extras/codeWars/decodeMorseCode.js
@@ -1,43 +1,36 @@
 decodeMorse = function(morseCode){
-    // console.log(MorseCode.get(".--")) 
+  
 
-//   let toDecode = []
-//   morseCode.split("").map((v,i)=>{  
+decodeMorse = function(morseCode){
+  
+    let setCode = morseCode.split(" ")  
+    
+    let ans = []
+    
+    setCode.map((v,i)=>{  
+    
+        if(v == "" && setCode[i+1] == "") {  
+            setCode.splice(i, 1)  
+        }
+    })  
+      
+    setCode.map((v,i)=>{ 
+    
+      if (v == "") { 
+        ans.push(" ")
+        } else {  
+          
+          ans.push(MORSE_CODE[v])
+          }
+      
+    }) 
+      
+      return ans.join("").trim()
+      
+      
+    }  
+}
 
-//     let start 
-//     let end 
-
-//     if (!v === "" && !start) { 
-//         start = i 
-//     } else if (morseCode.split("")[i+1] === "") {  
-//         end = i
-//     } else if (v === "" && morseCode.split("")[i+1] == "") {  
-//         toDecode.push("")
-//     }
-
-//   })  
-
-// console.log(morseCode.split(" ").length) 
-
-let setCode = morseCode.split(" ") 
-
-setCode.map((v,i)=>{  
-
-    if(v == "" && setCode[i+1] == "") {  
-        setCode.splice(i, 1) 
-    }
-}) 
-
-  }  
-
-//   let test 
-
-//   test ? console.log("its true") :  console.log("nah")
 
   decodeMorse('.... . -.--   .--- ..- -.. .') 
 
-//   arr = [1,2,3,4] 
-
-// arr.splice(0,2)
-
-//   console.log(arr)
