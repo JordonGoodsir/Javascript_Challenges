b8cca2e3cdf3696399811e2e438b86007d97bbf0
JordonGoodsir <jordon.goodsir1@gmail.com>
2021-04-27 10:54:54 +1000

    challenge halfway complete

--- extras/codeWars/decodeMorseCode.js
+++ extras/codeWars/decodeMorseCode.js
@@ -1,3 +1,43 @@
 decodeMorse = function(morseCode){
-    console.log(MorseCode.get(".--"))
-  }
+    // console.log(MorseCode.get(".--")) 
+
+//   let toDecode = []
+//   morseCode.split("").map((v,i)=>{  
+
+//     let start 
+//     let end 
+
+//     if (!v === "" && !start) { 
+//         start = i 
+//     } else if (morseCode.split("")[i+1] === "") {  
+//         end = i
+//     } else if (v === "" && morseCode.split("")[i+1] == "") {  
+//         toDecode.push("")
+//     }
+
+//   })  
+
+// console.log(morseCode.split(" ").length) 
+
+let setCode = morseCode.split(" ") 
+
+setCode.map((v,i)=>{  
+
+    if(v == "" && setCode[i+1] == "") {  
+        setCode.splice(i, 1) 
+    }
+}) 
+
+  }  
+
+//   let test 
+
+//   test ? console.log("its true") :  console.log("nah")
+
+  decodeMorse('.... . -.--   .--- ..- -.. .') 
+
+//   arr = [1,2,3,4] 
+
+// arr.splice(0,2)
+
+//   console.log(arr)
